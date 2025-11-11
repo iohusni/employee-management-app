eef6268eca88e80dfed1beda85f6cd98606137f4
iohusni <xuseenahmed132@gmail.com>
2025-11-11 20:41:29 +0300

    update a backend

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI  # type: ignore
+from fastapi.middleware.cors import CORSMiddleware  # type: ignore
 from db.connect_db import engine, Base
 from controller import employee_controller
 
 app = FastAPI(title="Employee API")
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Create tables on startup
 @app.on_event("startup")
