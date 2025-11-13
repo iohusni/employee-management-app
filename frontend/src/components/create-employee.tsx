cce721b9895130586f2e026e194214cda5c91558
iohusni <xuseenahmed132@gmail.com>
2025-11-13 20:15:07 +0300

    create form employee and validation

--- frontend/src/components/create-employee.tsx
+++ frontend/src/components/create-employee.tsx
@@ -1,54 +1,65 @@
 import { Button } from '@/components/ui/button'
 import {
   Dialog,
-  DialogClose,
   DialogContent,
   DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
 } from '@/components/ui/dialog'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
+
+import {
+  createEmployeeSchema,
+  defaultEmployeeSchema,
+  type createEmployeeSchemaType,
+} from '@/schema/employee-schema'
 import { useEmployeeStore } from '@/store/employee-store'
+import { FormProvider, useForm, type SubmitHandler } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import FormField from './form-field'
 
 const CreateEmployees = () => {
   const { isOpen, setIsOpen } = useEmployeeStore()
 
+  const form = useForm<createEmployeeSchemaType>({
+    resolver: zodResolver(createEmployeeSchema),
+    defaultValues: defaultEmployeeSchema,
+  })
+
+  const onSubmit: SubmitHandler<createEmployeeSchemaType> = (data) => {
+    console.log(data)
+  }
+
   return (
     <>
       <Dialog open={isOpen} onOpenChange={setIsOpen}>
-        <form>
-          <DialogContent className="sm:max-w-[425px]">
-            <DialogHeader>
-              <DialogTitle>Edit profile</DialogTitle>
-              <DialogDescription>
-                Make changes to your profile here. Click save when you&apos;re
-                done.
-              </DialogDescription>
-            </DialogHeader>
-            <div className="grid gap-4">
-              <div className="grid gap-3">
-                <Label htmlFor="name-1">Name</Label>
-                <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
+        <DialogContent className="sm:max-w-[500px]">
+          <DialogHeader>
+            <DialogTitle>Create Employee</DialogTitle>
+            <DialogDescription>
+              Create a new employee by filling out the form below.
+            </DialogDescription>
+          </DialogHeader>
+
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormProvider {...form}>
+              <div className=" space-y-4">
+                <FormField name="name" label="Name" />
+                <FormField name="email" label="Email" />
+                <FormField name="phone" label="Phone" />
+                <FormField name="address" label="Address" />
+                <FormField name="city" label="City" />
+                <FormField name="state" label="State" />
+
+                <DialogFooter>
+                  <Button type="submit" className="w-full">
+                    Create Employee
+                  </Button>
+                </DialogFooter>
               </div>
-              <div className="grid gap-3">
-                <Label htmlFor="username-1">Username</Label>
-                <Input
-                  id="username-1"
-                  name="username"
-                  defaultValue="@peduarte"
-                />
-              </div>
-            </div>
-            <DialogFooter>
-              <DialogClose asChild>
-                <Button variant="outline">Cancel</Button>
-              </DialogClose>
-              <Button type="submit">Save changes</Button>
-            </DialogFooter>
-          </DialogContent>
-        </form>
+            </FormProvider>
+          </form>
+        </DialogContent>
       </Dialog>
     </>
   )
